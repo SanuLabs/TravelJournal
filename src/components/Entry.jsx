8572e0660dd1d4f861d8fb10f7d0e4d501907b75
SanuLabs <muhammedsiyam868@gmail.com>
2025-04-29 23:48:56 +0530

    Implemented props

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,19 +1,18 @@
 import Marker from '../assets/location.svg'
-import Fuji from '../assets/fuji.png'
 
-export default function Entry(){
+export default function Entry(props){
     return(
         <article className='entry'>
-         <img src={Fuji} alt='fuji.png'></img>
+         <img src={props.img} alt='image'></img>
          <div className='entry-content'>
           <div className='location'>
               <img src={Marker} alt="location"></img>
-              <h3>JAPAN</h3>
-              <a href='https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu'>View on Google Maps</a>
+              <h3>{props.country}</h3>
+              <a href={props.location}>View on Google Maps</a>
           </div>
-          <h1>Mount Fuji</h1>
-          <h4>12 Jan, 2023 - 24 Jan, 2023</h4>
-          <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+          <h1>{props.name}</h1>
+          <h4>{props.date}</h4>
+          <p>{props.article}</p>
          </div>
         </article>
 
