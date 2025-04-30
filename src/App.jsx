e49bc43a466846492d62f703ffb8453e942e1e83
SanuLabs <muhammedsiyam868@gmail.com>
2025-04-30 23:27:06 +0530

    Spread object as props

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const entryElements = data.map(entry => {
     return(
         <Entry 
         key={entry.id}
-        entry={entry}
+        {...entry}
         />
       
     )
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -3,16 +3,16 @@ import Marker from '../assets/location.svg'
 export default function Entry(props){
     return(
         <article className='entry'>
-         <img src={props.entry.img.src} alt='image'></img>
+         <img src={props.img.src} alt='image'></img>
          <div className='entry-content'>
           <div className='location'>
               <img src={Marker} alt="location"></img>
-              <h3>{props.entry.country}</h3>
-              <a href={props.entry.googleMapsLink}>View on Google Maps</a>
+              <h3>{props.country}</h3>
+              <a href={props.googleMapsLink}>View on Google Maps</a>
           </div>
-          <h1>{props.entry.title}</h1>
-          <h4>{props.entry.dates}</h4>
-          <p>{props.entry.text}</p>
+          <h1>{props.title}</h1>
+          <h4>{props.dates}</h4>
+          <p>{props.text}</p>
          </div>
         </article>
 
